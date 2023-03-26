09330c1f747928d0a507b2ef5724af1b2fbf4a53
HirokiYasui <hiromaily2@gmail.com>
2023-03-26 11:13:10 +0900

    refactoring

---
+++ pkg/apis/eliza.go
@@ -0,0 +1,85 @@
+package apis
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/bufbuild/connect-go"
+
+	elizav1 "github.com/hiromaily/connect-example/pkg/gen/eliza/v1"
+)
+
+// Refer to
+// https://github.com/bufbuild/connect-demo/blob/3a30d4de07d6ac42110acd4ebf64bb4bf8a62579/main.go
+
+type ElizaServer struct {
+	// The time to sleep between sending responses on a stream
+	streamDelay time.Duration
+}
+
+func (e *ElizaServer) Say(
+	ctx context.Context,
+	req *connect.Request[elizav1.SayRequest],
+) (*connect.Response[elizav1.SayResponse], error) {
+	//reply, _ := eliza.Reply(req.Msg.Sentence) // ignore end-of-conversation detection
+	reply := fmt.Sprintf("You said, %s!", req.Msg.Sentence)
+	return connect.NewResponse(&elizav1.SayResponse{
+		Sentence: reply,
+	}), nil
+}
+
+func (e *ElizaServer) Converse(
+	ctx context.Context,
+	stream *connect.BidiStream[elizav1.ConverseRequest, elizav1.ConverseResponse],
+) error {
+	for {
+		if err := ctx.Err(); err != nil {
+			return err
+		}
+		request, err := stream.Receive()
+		if err != nil && errors.Is(err, io.EOF) {
+			return nil
+		} else if err != nil {
+			return fmt.Errorf("receive request: %w", err)
+		}
+		//reply, endSession := eliza.Reply(request.Sentence)
+		reply := fmt.Sprintf("You said, %s!", request.Sentence)
+		if err := stream.Send(&elizav1.ConverseResponse{Sentence: reply}); err != nil {
+			return fmt.Errorf("send response: %w", err)
+		}
+	}
+}
+
+func (e *ElizaServer) Introduce(
+	ctx context.Context,
+	req *connect.Request[elizav1.IntroduceRequest],
+	stream *connect.ServerStream[elizav1.IntroduceResponse],
+) error {
+	name := req.Msg.Name
+	if name == "" {
+		name = "Anonymous User"
+	}
+	//intros := eliza.GetIntroResponses(name)
+	intros := []string{"Hello", "Goodbye", "How are you"}
+	var ticker *time.Ticker
+	if e.streamDelay > 0 {
+		ticker = time.NewTicker(e.streamDelay)
+		defer ticker.Stop()
+	}
+	for _, resp := range intros {
+		if ticker != nil {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-ticker.C:
+			}
+		}
+		if err := stream.Send(&elizav1.IntroduceResponse{Sentence: resp}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
